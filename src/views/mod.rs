c5fd97761d3e8207f1c0c0e92cd42ef26afe5cb3
dujiajiyi <towangyanqing@gmail.com>
2016-07-29 00:20:05 +0800

    去掉ship的黄色背景， 添加天空background

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 use phi::gfx::{CopySprite, Sprite};
 use sdl2::render::{Texture, TextureQuery};
 use sdl2_image::LoadTexture;
+use sdl2::render::Renderer;
 
 /// Pixels traveled by the player's ship every second, when it is moving.
 const PLAYER_SPEED: f64 = 180.0;
@@ -27,6 +28,46 @@ enum ShipFrame {
     DownSlow = 8
 }
 
+#[derive(Clone)]
+struct Background {
+    pos: f64,
+    // The amount of pixels moved to the left every second
+    vel: f64,
+    sprite: Sprite,
+}
+
+impl Background {
+    fn render(&mut self, renderer: &mut Renderer, elapsed: f64) {
+        // We define a logical position as depending solely on the time and the
+        // dimensions of the image, not on the screen's size.
+        let size = self.sprite.size();
+        self.pos += self.vel * elapsed;
+        if self.pos > size.0 {
+            self.pos -= size.0;
+        }
+
+        // We determine the scale ratio of the window to the sprite.
+        let (win_w, win_h) = renderer.output_size().unwrap();
+        let scale = win_h as f64 / size.1;
+
+        // We render as many copies of the background as necessary to fill
+        // the screen.
+        let mut physical_left = -self.pos * scale;
+
+        while physical_left < win_w as f64 {
+            //? While the left of the image is still inside of the window...
+            renderer.copy_sprite(&self.sprite, Rectangle {
+                x: physical_left,
+                y: 0.0,
+                w: size.0 * scale,
+                h: win_h as f64,
+            });
+
+            physical_left += size.0 * scale;
+        }
+    }
+}
+
 struct Ship {
     rect: Rectangle,
     sprites: Vec<Sprite>,
@@ -35,6 +76,9 @@ struct Ship {
 
 pub struct ShipView {
     player: Ship,
+    bg_back: Background,
+    bg_middle: Background,
+    bg_front: Background,
 }
 
 impl ShipView {
@@ -63,7 +107,22 @@ impl ShipView {
                 },
                 sprites: sprites,
                 current: ShipFrame::MidNorm
-            }
+            },
+            bg_back: Background {
+                pos: 0.0,
+                vel: 20.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starBG.png").unwrap(),
+            },
+            bg_middle: Background {
+                pos: 0.0,
+                vel: 40.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starMG.png").unwrap(),
+            },
+            bg_front: Background {
+                pos: 0.0,
+                vel: 80.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starFG.png").unwrap(),
+            },
         }
     }
 }
@@ -134,15 +193,30 @@ impl View for ShipView {
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
         phi.renderer.clear();
 
-        // Render the scene
-        phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
-        phi.renderer.fill_rect(self.player.rect.to_sdl().unwrap());
+        // Render the scene 去掉ship的背景
+        //phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+        //phi.renderer.fill_rect(self.player.rect.to_sdl().unwrap());
 
+        // Render the Backgrounds
+        self.bg_back.render(&mut phi.renderer, elapsed);
+        self.bg_middle.render(&mut phi.renderer, elapsed);
 
+        const DEBUG: bool = false;
+
+        // Render the bounding box (for debugging purposes)
+        if DEBUG {
+            phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+            phi.renderer.fill_rect(self.player.rect.to_sdl().unwrap());
+        }
+
+        // Render the ship
         phi.renderer.copy_sprite(
             &self.player.sprites[self.player.current as usize],
             self.player.rect
         );
+
+        // Render the foreground
+        self.bg_front.render(&mut phi.renderer, elapsed);
 
         ViewAction::None
     }
